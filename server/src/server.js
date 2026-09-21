458f14c2958040b50073bace47ed66e7b82d9dd5
vaishali verma <vaishaliverma288@gmail.com>
2026-09-21 18:04:48 +0530

    updated server.js

--- server/src/server.js
+++ server/src/server.js
@@ -39,7 +39,7 @@ const startServer = async () => {
     }
 
     // 3️⃣ ALWAYS listen (Render requires this)
-    server.listen(PORT, () => {
+    server.listen(PORT,"0.0.0.0", () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
   } catch (error) {
